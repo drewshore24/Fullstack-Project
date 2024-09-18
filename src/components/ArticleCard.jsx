aa7b9fc88f92e5c36c1be4ba9fab7590f5bf8a2e
Drew <andrewshore123@hotmail.com>
2024-09-18 13:38:01 +0100

    Q7 Vote on an article

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom";
 
 const ArticleCard = ({ article}) => {
+  const newDate = new Date(article.created_at).toString()
     return(
     <section className="Article">
     <p>Article id: {article.article_id}</p>
     <p>Title: {article.title}</p>
     <p>Topic: {article.topic}</p>
     <p>Author: {article.author}</p>
-    <p>Created_at: {article.created_at}</p>
+    <p>Created_at: {newDate}</p>
     <p>Votes: {article.votes}</p>
     <p>Comment count: {article.comment_count}</p>
     <img src={article.article_img_url} alt="image" />
